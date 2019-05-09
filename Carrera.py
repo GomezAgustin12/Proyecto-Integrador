3dec8ecc7961ac0660cb60b5fb4b985c5ce619d1
GomezAgustin <agustingmez@gmail.com>
2019-05-09 13:43:18 -0300

    Cree clases Carrera y Facultad

---
+++ Carrera.py
@@ -0,0 +1,5 @@
+
+class Carrera:
+    def __init__(self, nombre):
+        self.nombre=nombre
+
---
+++ Facultad.py
@@ -0,0 +1,18 @@
+import Carrera
+Carrera=Carrera.Carrera
+
+class Facultad:
+    def __init__(self, nombre):
+        self.nombre= nombre
+        self.carreras=[]
+
+    def CrearCarrera(self, nombre):
+        aux= Carrera(nombre)
+        self.carreras.append(aux)
+
+
+ing = Facultad("Ingenieria y Tecnologia")
+ing.CrearCarrera("Ingenieria en sistemas")
+print(ing.carreras[0].nombre)
+
+
