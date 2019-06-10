ff3fdb4a9898feed3d8bb14b828486e8bc21b22d
GomezAgustin <agustingmez@gmail.com>
2019-06-10 12:45:18 -0300

    Se creo la clase Inicio y se creo el metodo carga el cual se encarga de cargar todos los datos al inicio del programa

--- Facultad.py
+++ Facultad.py
@@ -6,11 +6,6 @@ class Facultad:
         self.nombre= nombre
         self.carreras=[]
 
-    def CrearCarrera(self, nombre):
+    def crearCarrera(self, nombre):
         aux= Carrera(nombre)
         self.carreras.append(aux)
-
-
-ing = Facultad("Ingenieria y Tecnologia")
-ing.CrearCarrera("Ingenieria en sistemas")
-print(ing.carreras[0].nombre)
